f815fb871a710fb1aeab8650116ca8784ed0f801
Brandon Lew <brandon.t.lew@gmail.com>
2018-01-25 22:36:53 -0800

    new color

--- styles/Constants.js
+++ styles/Constants.js
@@ -2,5 +2,6 @@
 export const colorDarkGrey = '#494F56';
 export const colorLightGrey = "#F0F0F0";
 export const colorOrange = '#B57A42';
+export const colorLightOrange = '#ECDED0';
 export const colorBlue = '#427DB5';
 export const colorLightBlue = '#D0DFEC';
